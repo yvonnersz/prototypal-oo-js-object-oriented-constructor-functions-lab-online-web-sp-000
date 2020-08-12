c030da8519fa09d31312c7a5ded8bc0700a5c142
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-12 09:27:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function Scooter(year, color, model) {
   this.color = color;
   this.model = model;
 }
+
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
